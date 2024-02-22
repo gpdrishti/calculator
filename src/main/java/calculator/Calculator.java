e6f05c31da025ae739ec5a1bf3d9925ddd7a4624
gpdrishti <141937800+gpdrishti@users.noreply.github.com>
2024-02-22 21:29:34 +0530

    Update Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import java.util;
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
